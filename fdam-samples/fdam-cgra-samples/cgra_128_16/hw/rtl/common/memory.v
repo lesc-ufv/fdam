48f519db108725cfc8c44bb532d766e1cdb567d8
Lucas Bragança <lucas@k40pontenova.k40.pontenova>
2018-11-20 23:28:59 -0200

    adicionado exemplo cgra 128 PEs sintezado

---
+++ fdam-samples/fdam-cgra-samples/cgra_128_16/hw/rtl/common/memory.v
@@ -0,0 +1,38 @@
+module memory #
+(
+  parameter DATA_WIDTH = 32,
+  parameter ADDR_WIDTH = 8
+)
+(
+  input clk,
+  input we,
+  input re,
+  input [ADDR_WIDTH-1:0] raddr,
+  input [ADDR_WIDTH-1:0] waddr,
+  input [DATA_WIDTH-1:0] din,
+  output reg [DATA_WIDTH-1:0] dout
+);
+
+  reg [DATA_WIDTH-1:0] mem [0:2**ADDR_WIDTH-1];
+  //synthesis translate_off
+  integer i;
+
+  initial begin
+    dout = 0;
+    for(i=0; i<2**ADDR_WIDTH; i=i+1) begin
+      mem[i] = 0;
+    end
+  end
+
+  //synthesis translate_on
+
+  always @(posedge clk) begin
+    if(we) begin
+      mem[waddr] <= din;
+    end 
+    if(re) begin
+      dout <= mem[raddr];
+    end 
+  end
+
+endmodule
