7ec4b7c254560c5e57483d97f1eec5a367faaeaa
M. Canesche <canesche>
2019-04-09 10:34:33 -0300

    correct in create the graph Chebyshev balanced

--- fdam-samples/fdam-cgra-samples/cgra_128_radix8/sw/cpp/src/Chebyshev.cpp
+++ fdam-samples/fdam-cgra-samples/cgra_128_radix8/sw/cpp/src/Chebyshev.cpp
@@ -23,6 +23,8 @@ DataFlow *Chebyshev::createDataFlow(int id) {
     auto reg2 = new PassB(idx++);
     auto reg3 = new PassB(idx++);
     auto reg4 = new PassB(idx++);
+    auto reg5 = new PassB(idx++);
+    auto reg6 = new PassB(idx++);
     auto mult1 = new Multi(idx++, 16);
     auto mult2 = new Mult(idx++);
     auto sub1 = new Subi(idx++, 20);
@@ -38,16 +40,18 @@ DataFlow *Chebyshev::createDataFlow(int id) {
     df->connect(reg1, reg2, reg2->getPortA());
     df->connect(reg2, reg3, reg3->getPortA());
     df->connect(reg3, reg4, reg4->getPortA());
+    df->connect(reg4, reg5, reg4->getPortA());
+    df->connect(reg5, reg6, reg4->getPortA());
 
     df->connect(reg1, mult2, mult2->getPortA());
     df->connect(mult1, mult2, mult2->getPortB());
     df->connect(mult2, sub1, sub1->getPortA());
-    df->connect(reg2, mult3, mult3->getPortA());
+    df->connect(reg3, mult3, mult3->getPortA());
     df->connect(sub1, mult3, mult3->getPortB());
-    df->connect(reg3, mult4, mult4->getPortA());
+    df->connect(reg4, mult4, mult4->getPortA());
     df->connect(mult3, mult4, mult4->getPortB());
     df->connect(mult4, add1, add1->getPortA());
-    df->connect(reg4, mult5, mult5->getPortA());
+    df->connect(reg6, mult5, mult5->getPortA());
     df->connect(add1, mult5, mult5->getPortB());
 
     df->connect(mult5, out, out->getPortA());
