74622c7cf7562ed613c722aaa305712ddd8d2939
canesche <michael.canesche@gmail.com>
2019-04-12 13:28:49 -0300

    poly5 verify

--- fdam-samples/fdam-cgra-samples/cgra_128_radix8/sw/cpp/src/Poly5.cpp
+++ fdam-samples/fdam-cgra-samples/cgra_128_radix8/sw/cpp/src/Poly5.cpp
@@ -18,9 +18,9 @@ Poly5::~Poly5() = default;
 DataFlow *Poly5::createDataFlow(int id) {
     auto df = new DataFlow(id, "Poly5");
     int idx = 0;
+    auto in0 = new InputStream(idx++);
     auto in1 = new InputStream(idx++);
     auto in2 = new InputStream(idx++);
-    auto in3 = new InputStream(idx++);
     auto reg1 = new PassB(idx++);
     auto reg2 = new PassB(idx++);
     auto reg3 = new PassB(idx++);
@@ -30,7 +30,6 @@ DataFlow *Poly5::createDataFlow(int id) {
     auto reg7 = new PassB(idx++);
     auto reg8 = new PassB(idx++);
     auto reg9 = new PassB(idx++);
-    auto reg20 = new PassB(idx++);
     auto reg10 = new PassB(idx++);
     auto reg11 = new PassB(idx++);
     auto reg12 = new PassB(idx++);
@@ -41,6 +40,25 @@ DataFlow *Poly5::createDataFlow(int id) {
     auto reg17 = new PassB(idx++);
     auto reg18 = new PassB(idx++);
     auto reg19 = new PassB(idx++);
+    auto reg20 = new PassB(idx++);
+    auto reg21 = new PassB(idx++);
+    auto reg22 = new PassB(idx++);
+    auto reg23 = new PassB(idx++);
+    auto reg24 = new PassB(idx++);
+    auto reg25 = new PassB(idx++);
+    auto reg26 = new PassB(idx++);
+    auto reg27 = new PassB(idx++);
+    auto reg28 = new PassB(idx++);
+    auto reg29 = new PassB(idx++);
+    auto reg30 = new PassB(idx++);
+    auto reg31 = new PassB(idx++);
+    auto reg32 = new PassB(idx++);
+    auto reg33 = new PassB(idx++);
+    auto reg34 = new PassB(idx++);
+    auto reg35 = new PassB(idx++);
+    auto reg36 = new PassB(idx++);
+    auto reg37 = new PassB(idx++);
+    auto reg38 = new PassB(idx++);
     auto add = new Add(idx++);
     auto sub1 = new Subi(idx++, 432);
     auto sub2 = new Subi(idx++, 207);
@@ -60,33 +78,112 @@ DataFlow *Poly5::createDataFlow(int id) {
     auto mult8 = new Mult(idx++);
     auto mult9 = new Mult(idx++);
     auto add4 = new Sub(idx++);
-    auto sub5 = new Add(idx++);
+    auto sub5 = new Sub(idx++);
     auto sub6 = new Subi(idx++, 298584);
     auto sub7 = new Subi(idx++, 248832);
-    auto mult13 = new Mult(idx++);
     auto mult10 = new Mult(idx++);
     auto mult11 = new Mult(idx++);
-    auto add5 = new Sub(idx++);
-    auto add6 = new Sub(idx++);
+    auto add5 = new Add(idx++);
+    auto add6 = new Add(idx++);
+    auto add7 = new Add(idx++);
     auto mult12 = new Mult(idx++);
+    auto mult13 = new Mult(idx++);
     auto out = new OutputStream(idx++);
 
-    df->connect(in3, sub1, sub1->getPortA());
-    df->connect(in3, sub2, sub2->getPortA());
-    df->connect(in3, mult1, mult1->getPortA());
-    df->connect(in3, mult2, mult2->getPortA());
-    df->connect(in3, mult3, mult3->getPortA());
-    df->connect(in3, reg1, reg1->getPortA());
+    // level 1
+    df->connect(in2, reg1, reg1->getPortB());
+    df->connect(in2, reg2, reg2->getPortB());
+    df->connect(in2, sub1, sub1->getPortA());
+    df->connect(in2, reg3, reg3->getPortB());
+    df->connect(in2, mult1, mult1->getPortA());
+    df->connect(in2, mult2, mult2->getPortA());
+    df->connect(in2, reg4, reg4->getPortB());
+    df->connect(in2, sub2, sub2->getPortA());
+    df->connect(in2, mult3, mult3->getPortA());
+    df->connect(in0, reg5, reg5->getPortB());
+    df->connect(in0, reg6, reg6->getPortB());
+    df->connect(in0, reg7, reg7->getPortB());
+    df->connect(in0, reg8, reg8->getPortB());
+    df->connect(in1, add1, add1->getPortA());
+    df->connect(in1, reg9, reg9->getPortB());
+    df->connect(in1, reg10, reg10->getPortB());
+    // level 2
+    df->connect(reg1, reg11, reg11->getPortB());
+    df->connect(reg2, mult4, mult4->getPortB());
+    df->connect(sub1, mult4, mult4->getPortA());
+    df->connect(reg3, reg12, reg12->getPortB());
+    df->connect(mult1, sub3, sub3->getPortA());
+    df->connect(mult2, sub4, sub4->getPortA());
+    df->connect(reg4, mult4, mult4->getPortB());
+    df->connect(sub2, mult4, mult4->getPortA());
+    df->connect(mult3, sub41, sub41->getPortB());
+    df->connect(reg5, reg13, reg13->getPortB());
+    df->connect(reg6, reg14, reg14->getPortB());
+    df->connect(reg7, reg15, reg15->getPortB());
+    df->connect(reg8, reg16, reg16->getPortB());
+    df->connect(add1, sub41, sub41->getPortA());
+    df->connect(reg9, reg17, reg17->getPortB());
+    df->connect(reg10, reg18, reg18->getPortB());
+    // level 3
+    df->connect(reg11, reg20, reg20->getPortB());
+    df->connect(mult4, add2, add2->getPortA());
+    df->connect(reg12, mult5, mult5->getPortB());
+    df->connect(sub3, mult5, mult5->getPortA());
+    df->connect(sub4, mult6, mult6->getPortA());
+    df->connect(mult4, add3, add3->getPortA());
+    df->connect(sub41, mult7, mult7->getPortA());
+    df->connect(reg13, mult6, mult6->getPortB());
+    df->connect(reg14, reg21, reg21->getPortB());
+    df->connect(reg15, mult7, mult7->getPortB());
+    df->connect(reg16, reg22, reg22->getPortB());
+    df->connect(reg17, reg23, reg23->getPortB());
+    df->connect(reg18, reg24, reg24->getPortB());
+    // level 4
+    df->connect(reg20, mult9, mult9->getPortA());
+    df->connect(add2, mult9, mult9->getPortB());
+    df->connect(mult5, add5, add5->getPortA());
+    df->connect(mult6, add5, add5->getPortB());
+    df->connect(add3, sub5, sub5->getPortA());
+    df->connect(mult7, sub5, sub5->getPortB());
+    df->connect(reg21, reg25, reg25->getPortB());
+    df->connect(reg22, reg26, reg26->getPortB());
+    df->connect(reg23, reg27, reg27->getPortB());
+    df->connect(reg24, reg28, reg28->getPortB());
+    // level 5
+    df->connect(mult9, sub6, sub6->getPortA());
+    df->connect(add5, sub7, sub7->getPortA());
+    df->connect(sub5, mult10, mult10->getPortA());
+    df->connect(reg25, mult10, mult10->getPortB());
+    df->connect(reg26, reg29, reg29->getPortB());
+    df->connect(reg27, reg30, reg30->getPortB());
+    df->connect(reg28, reg31, reg31->getPortB());
+    // level 6
+    df->connect(sub6, reg32, reg32->getPortB());
+    df->connect(sub7, mult11, mult11->getPortA());
+    df->connect(mult10, mult12, mult12->getPortA());
+    df->connect(reg29, mult11, mult11->getPortB());
+    df->connect(reg31, reg34, reg34->getPortB());
+    // level 7
+    df->connect(reg30, mult12, mult12->getPortB());
+    df->connect(reg32, add6, add6->getPortB());
+    df->connect(mult11, add6, add6->getPortA());
+    df->connect(mult12, reg36, reg36->getPortB());
+    df->connect(add6, add7, add7->getPortA());
+    df->connect(reg36, add7, add7->getPortB());
+
+    df->connect(add7, mult13, mult13->getPortA());
+    df->connect(reg34, reg37, reg37->getPortB());
+    df->connect(reg37, reg38, reg38->getPortB());
+
+    df->connect(reg38, mult13, mult13->getPortB());
+    df->connect(mult13, out, out->getPortA());
+    
+
+/*
+    
     df->connect(reg1, reg2, reg2->getPortA());
     df->connect(reg2, reg3, reg3->getPortA());
-    df->connect(reg3, reg4, reg4->getPortA());
-
-    df->connect(in1, reg5, reg5->getPortA());
-    df->connect(reg5, reg6, reg6->getPortA());
-    df->connect(reg6, reg7, reg7->getPortA());
-    df->connect(reg7, reg8, reg8->getPortA());
-    df->connect(reg8, reg9, reg9->getPortA());
-    df->connect(reg9, reg20, reg20->getPortA());
+    
 
     df->connect(in2, add1, add1->getPortA());
     df->connect(in2, reg10, reg10->getPortA());
@@ -150,6 +247,7 @@ DataFlow *Poly5::createDataFlow(int id) {
     df->connect(add6, mult12, mult12->getPortB());
 
     df->connect(mult12, out, out->getPortA());
+    */
     return df;
 }
 
